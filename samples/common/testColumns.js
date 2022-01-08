e5b819f2c52dfe5e8750260fbb511f52fae08c9a
anuj.mayank <anuj.mayank@bottomline.com>
2022-01-08 18:32:45 +0530

    Initial commit

---
+++ samples/common/testColumns.js
@@ -0,0 +1,112 @@
+const testCol = [
+   {
+      id: "groupId",
+      width: 100,
+      header: [
+         {
+            text: "CoP ID",
+            toolTip: null
+         },
+         {
+            content: "inputFilter"
+         }
+      ],
+      type: "string",
+      align: "left",
+      resizable: true,
+      editing: false,
+      sortable: true,
+      tooltip: true
+   },
+   {
+      id: "name",
+      width: 200,
+      header: [
+         {
+            text: "CoP Name",
+            toolTip: null
+         },
+         {
+            content: "inputFilter"
+         }
+      ],
+      type: "string",
+      align: "left",
+      resizable: true,
+      editing: false,
+      sortable: true
+   },
+   {
+      id: "description",
+      width: 300,
+      header: [
+         {
+            text: "CoP Description",
+            toolTip: null
+         },
+         {
+            content: "inputFilter"
+         }
+      ],
+      type: "string",
+      align: "left",
+      resizable: true,
+      editing: false,
+      sortable: true
+   },
+   {
+      id: "category",
+      width: 150,
+      header: [
+         {
+            text: "CoP Category",
+            toolTip: null
+         },
+         {
+            content: "inputFilter"
+         }
+      ],
+      type: "string",
+      align: "left",
+      resizable: true,
+      editing: false,
+      sortable: true
+   },
+   {
+      id: "creationDate",
+      width: 130,
+      header: [
+         {
+            text: "Creation Date",
+            toolTip: null
+         },
+         {
+            content: "inputFilter"
+         }
+      ],
+      type: "date",
+      format: "%d-%M-%Y",
+      align: "center",
+      resizable: true,
+      editing: false,
+      sortable: true
+   },
+   {
+      id: "formattedKpacsCounter",
+      width: 180,
+      header: [
+         {
+            text: "Number of K-PACs",
+            toolTip: null
+         },
+         {
+            content: "inputFilter"
+         }
+      ],
+      type: "string",
+      align: "center",
+      resizable: true,
+      editing: false,
+      sortable: true
+   }
+];
